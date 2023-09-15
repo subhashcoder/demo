88b66d8f3cb5eca60f1d9c95489630a12b2fb85f
subhash saket <144803479+subhashcoder@users.noreply.github.com>
2023-09-15 11:47:47 +0000

    all variable and some data type

--- js/test.js
+++ js/test.js
@@ -1,1 +1,27 @@
 console.log(10)
+"use strict" // treat all  js code as newer version 
+  
+  const a = "subhash"
+  let b ="satish"
+  var subhash="rewa"
+  a=10
+  console.table([a,b])
+
+console.log("hello i am subhash")
+alert(3) //we are using node js, not browser
+//data type
+//prinitive data type
+//number => 2 to power 53
+//bigint => for use to large number 
+//string => single and double qoutation
+//boolean => ture/false
+//null  =>  standalone value
+//undifine=> we have a declera variable but not asign a value
+// symbol => for use qnuiqe 
+
+
+//object
+console.log(typeof "subhash") // output=> string 
+console.log(typeof age) // output=> number
+console.log(typeof undefined) // output=> undifined
+console.log(typeof  null) // output=>  object
